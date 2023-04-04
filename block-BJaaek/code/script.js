a66bc8ecaa02ecd3f10e842f1d9da79f479a1b30
Akash <akashraaz61@gmail.com>
2023-04-04 23:17:28 +0530

    disco grid

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,14 +1,19 @@
-let firstBox = document.querySelector('.first');
-let secondBox = document.querySelector('.second');
+let boxes = document.querySelector('.boxes')
 
+for(let i=1;i<=500;i++){
+  let box = document.createElement('div');
+  let randomNumber = Math.floor((Math.random() * 500) + 1);
+  let randomColor = Math.floor(Math.random()*16777215).toString(16)
+  box.classList.add('box');
+  box.innerText = randomNumber;
+  box.style.backgroundColor = '#'+randomColor;
+  boxes.append(box)
 
+  box.addEventListener('mousemove',function(){
+    let randomNumber = Math.floor((Math.random() * 500) + 1);
+    let randomColor = Math.floor(Math.random()*16777215).toString(16)
+    box.innerText = randomNumber;
+    box.style.backgroundColor = '#'+randomColor;
+  })
+}
 
-firstBox.addEventListener('click', function(){
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  firstBox.style.backgroundColor = '#'+randomColor;
-})
-
-secondBox.addEventListener('mousemove', function(){
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  secondBox.style.backgroundColor = '#'+randomColor;
-})
