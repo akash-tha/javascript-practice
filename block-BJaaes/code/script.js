baf113aa79fcfbcc19bc6438e39a707bb8ef7b0b
Akash <akashraaz61@gmail.com>
2023-06-10 21:13:11 +0530

    form-validation

--- block-BJaaes/code/script.js
+++ block-BJaaes/code/script.js
@@ -1,75 +1,107 @@
+let form = document.querySelector('form')
 
+const doesContainNumber = (str) => str.split('').some(e=>Number(e));
 
-function doescontainaNumber(str)
-{
-    return str.split('').some(e=>Number(e))
+const validEmail = (str)=> str.includes('@');
+
+const checkIfAllNumber = (str)=> str.split('').every(e=>Number(e))
+
+const validLength= (str,len)=> {
+    console.log(str,len)
+    if(str.length>=len)
+        return true;
+    else 
+        return false;
+}
+
+let errorMessage = {};
+
+function displayError(name){
+    let elm = form.elements[name];
+    elm.nextElementSibling.innerText = errorMessage[name];
+    elm.classList.add("error");
+    elm.classList.remove("success");
+}
+
+function displaySuccess(name){
+    let elm = form.elements[name];
+    elm.nextElementSibling.innerText = "";
+    elm.classList.add("success");
+    elm.classList.remove("error");
+}
+
+function checkForBlank(str){
+    if(str.value===""){
+        errorMessage[str.name]= `${str.name} can't be blank`;
+        displayError(str.name)
+    } else {
+        errorMessage[str.name] = '';
+        displaySuccess(str.name)
+    }
 }
 
 
-
-let form=document.querySelector("form")
-
-errorMsg="";
-
-function function1(Event)
+function submitHandler(event)
 {
-    Event.preventDefault();
+    event.preventDefault();
+    let name=event.target.elements.name;
+    let email=event.target.elements.email;
+    let phone = event.target.elements.phone;
+    let password=event.target.elements.password;
+    let confirmpassword=event.target.elements.confirmpassword;
 
-    let name=Event.target.elements.name;
-    let email=Event.target.elements.email;
-    let password=Event.target.elements.password;
-    let confirmpassword=Event.target.elements.confirmpassword;
 
+    //Check if Name is blank.
+    checkForBlank(name);
 
-    function check(str)
-    {
-        if(str.value==="")
-        {
-            errorMsg="value can't be left blank";
-            str.classList.add("error");
-            str.classList.remove("success");
-        }
-       
-        else
-        {
-              errorMsg="";
-              str.classList.add("success");
-              str.classList.remove("error");
-        }
-        str.nextElementSibling.innerText=errorMsg;
- }
+    //Check if Email is blank
+    checkForBlank(email);
 
-     check(name);
-     check(email);
+    //Check if Passowrd is blank
+    checkForBlank(password);
 
+    //Check if confirm password is blank
+    checkForBlank(confirmpassword)
+
+    //Name can't be numbers
+    console.dir(name)
+    if(doesContainNumber(name.value)){
+        //console.log('name contain a number')
+        errorMessage.name="Name can't have numbers";
+        displayError('name')
+    }
+
+    //Email should contain symbol '@'
+    if(!validEmail(email.value)){
+        errorMessage.email = "Not a valid email";
+        displayError('email')
+    }
+    //Email must be atleast 6 characters
+    if(!validLength(email.value,6)){
+        errorMessage.email ="Email can't be less than 6 chars";
+        displayError('email')
+    }
+
+     //Phone number can only be a number
+     if(!checkIfAllNumber(phone.value)){
+        errorMessage.phone ="Phone can only be numbers";
+        displayError('phone')
+    }
+    
+    //Password should match confirmPassword
      if(password.value!==confirmpassword.value)
      {
-        errorMsg="password mismatch";
-        password.classList.add("error");
-        password.classList.remove("success");
+        errorMessage.password="password mismatch";
+        displayError('password')
      }
-     else if(!doescontainaNumber(password.value))
-    {
-        errorMsg="password must contain a numeric value";
-        password.classList.add("error");
-        password.classList.remove("success");
 
-
-    }
-     else
-     {
-        errorMsg="";
-        password.classList.add("success");
-        password.classList.remove("error");
-
+     //Password Must contain a number
+    if(!doesContainNumber(password.value)){
+        errorMessage.password="password must contain a numeric value";
+        displayError('password')
+    } else{
+        displaySuccess('password')
      }
-     confirmpassword.nextElementSibling.innerText=errorMsg;
-     console.log(password.value);
-     console.log(confirmpassword.value);
-
-   
-
 }
 
-
-form.addEventListener("submit",function1)
+form.addEventListener("submit",submitHandler)
