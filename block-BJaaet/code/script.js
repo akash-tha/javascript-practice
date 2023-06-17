1d64f7173f63c44dc255cc431c231a66bc63f7bb
Akash <akashraaz61@gmail.com>
2023-06-17 14:28:18 +0530

    movie watch list

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -1,110 +1,69 @@
-let input=document.querySelector('input[type="text"]')
-let ul=document.querySelector('.list')
-let buttons=document.querySelector('.buttons')
-let active=document.querySelector('.Active')
-let completed=document.querySelector('.Completed')
-let showall=document.querySelector('.ShowAll')
-let clearall=document.querySelector('.ClearAll')
-let error=document.querySelector('.errormsg')
+let ul = document.querySelector('.list')
 
+let form = document.querySelector('form')
 
-let errorMsg="";
+let btnAdd = document.querySelector('.Add')
 
-
-let movieInfo=JSON.parse(localStorage.getItem("record")) || [];
-
-function1(movieInfo,ul)
-
-input.addEventListener("keyup",(event)=>
-{
-  if(event.target.value==="")
-  {
-    errorMsg="input field can't be left blank";
-    error.innerText=errorMsg;
-  }
-  else if(event.keyCode===13)
-{
-    errorMsg="";
-    error.innerText=errorMsg;
-    movieInfo.push({
-    Name:event.target.value,
-    watched:false,
-})
-function1(movieInfo,ul)
-event.target.value="";
-localStorage.setItem("record",JSON.stringify(movieInfo))
-}
-})
-
-
-
-function toggle(event)
-  {
-    let id=event.target.dataset.id;
-    movieInfo[id].watched=!movieInfo[id].watched;
-    localStorage.setItem("record",JSON.stringify(movieInfo));
-    function1(movieInfo,ul);
-  }
-
-  function function1(movieInfo,ul)
-  {
-    ul.innerHTML="";
-    movieInfo.forEach((movie,index)=>
+let movies = [
     {
-    let li=document.createElement("li");
-    li.classList.add("blocks");
-    let input1=document.createElement("input");
-    input1.type="checkbox";
-    input1.addEventListener("input",toggle);
-    input1.setAttribute("data-id",index)
-    input1.checked=movie.watched;
-    input1.classList.add("checkboxx");
-    let label=document.createElement("label");
-    label.innerText=movie.Name;
-    let span1=document.createElement("span");
-    span1.innerText="❌";
-    span1.addEventListener("click",(event)=>
-     {
-      movieInfo.splice(index,1);
-      localStorage.setItem("record",JSON.stringify(movieInfo))
-      event.target.parentElement.remove();
-     })
-       li.append(input1,label,span1);
-       ul.append(li);
+        name: 'Inception',
+        watched: false
+    }
+]
+
+// console.log(btnAdd)
+// console.log(ul)
+
+function addMovie(event){
+    event.preventDefault();
+    //console.dir(event.target.elements.movie)
+
+    movies.push({
+        name: event.target.elements.movie.value,
+        watched: false
+}); 
+    createMovieUI();
+}
+
+function handleChange(event){
+    console.log(event.target)
+    const id = event.target.id;
+    movies[id].watched = !movies[id].watched
+    createMovieUI()
+}
+
+function createMovieUI(){
+    ul.innerHTML = "";
+    console.log(movies)
+    movies.forEach((movie,i)=>{
+        let li = document.createElement('li');
+        li.id = Math.random()
+        li.classList.add('blocks')
+        let input = document.createElement('input');
+        input.type = 'checkbox';
+        input.id = i;
+        input.addEventListener('change', handleChange)
+        let label = document.createElement('label');
+        label.for = i;
+        label.innerText = movie.name;
+        let span = document.createElement('span');
+        span.innerText = 'x';
+        span.setAttribute('data-id', i)
+        span.addEventListener('click', deleteMovie )
+        li.append(input,label, span)
+        ul.append(li);
     })
-  }
+}
 
- 
+function deleteMovie(event){
+    console.log('delete movie');
+    //event.target.parentElement.remove();
+   const id = event.target.dataset.id;
+   //delete 1 element at index id
+   movies.splice(id, 1)
+   createMovieUI();
+}
 
-  active.addEventListener("click", ()=>
-  {
-    let a=movieInfo.filter((movie)=>!movie.watched)
-    function1(a,ul);
-  })
-  completed.addEventListener("click", ()=>
-  {
-    let b=movieInfo.filter((movie)=>movie.watched)
-    function1(b,ul);
-  })
-  showall.addEventListener("click", ()=>
-  {
-    
-    function1(movieInfo,ul);
-  })
-  clearall.addEventListener("click", ()=>
-  {
-    
-    movieInfo=[];
-    function1(movieInfo,ul);
-  })
-  
+createMovieUI();
 
-  
-  
-
-
-
-    
-    
-
-
+form.addEventListener('submit', addMovie)
